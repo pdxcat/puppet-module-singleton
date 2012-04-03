61217cc0cabdd5496822fa4552d3a6dc4871d58c
Reid Vandewiele <marut@cat.pdx.edu>
2012-04-03 08:53:17 -0700

    Stub out new function singleton_resources

---
+++ lib/puppet/parser/functions/singleton_resources.rb
@@ -0,0 +1,94 @@
+Puppet::Parser::Functions::newfunction(:singleton_resources, :doc => <<-'ENDHEREDOC') do |args|
+    Create or include a resource defined either with defaults or with 
+    hiera-supplied parameters.
+
+    Example usage:
+
+    singleton_resources(Package['vim'], Package['emacs'], User['fu'])
+
+    By default, packages will be created with parameters specified in hiera
+    keys of the form:
+
+    singleton_resource_${type}_${title}
+
+    Additionally, default values can be specified using the hiera key
+
+    singleton_resource_${type}
+
+    The data should be in the form of a hash, which can include the keys:
+
+    parameters
+    include_singleton_resources
+    include_classes
+
+    Parameters should be a hash containing the parameters to give to the
+    resource.
+
+    include_singleton_resources should be an array of singleton_resources to
+    chain-include when the singleton_resource in question is called.
+
+    include_classes should be an array of classes to include when the 
+    singleton_resource in question is called.
+
+    For example (in YAML):
+
+    ---
+    :singleton_resource_package_vim:
+      :parameters:
+        :name: vim
+        :ensure: present
+      :include_singleton_resources:
+      - Package['vim-puppet']
+      :include_classes:
+      - stdlib
+      - obviouslyfakeclass
+
+  ENDHEREDOC
+
+  Puppet::Parser::Functions.autoloader.loadall
+
+  args.each do |resource|
+    next if function_defined(resource)
+
+    case resource
+    when String
+      # convert into resource
+    when Puppet::Resource
+      # yay
+    else
+      raise ArgumentError, "Invalid argument of type '#{val.class}' to 'singleton_resources'"
+    end
+
+    singleton_loaded = self.catalog.classes.include?('singleton')
+    function_include('singleton') unless singleton_loaded
+    scope = self.class_scope('singleton')
+
+    type  = resource.type.to_s
+    title = resource.title.to_s
+    defaults_key = "singleton_resource_#{type}"
+    resource_key = "singleton_resource_#{type}_#{title}"
+
+    defaults = scope.function_hiera(defaults_key)
+    config   = scope.function_hiera(resource_key, defaults_key)
+    config[:parameters]                   ||= {}
+    config[:include_singleton_packages]   ||= []
+    config[:include_classes]              ||= []
+    defaults[:parameters]                 ||= {}
+    defaults[:include_singleton_packages] ||= []
+    defaults[:include_classes]            ||= []
+
+    Puppet::Util.symbolizehash!(config)
+    Puppet::Util.symbolizehash!(defaults)
+
+    params = defaults[:parameters].merge(config[:parameters])
+    class_includes = defaults[:include_classes].merge(config[:include_classes])
+    singleton_includes = defaults[:include_singleton_resources].merge(config[:include_singleton_resources])
+    resource = { resource_key => params }
+    scope.function_create_resources([type, resource])
+
+    scope.function_singleton_packages(singleton_includes)
+    scope.function_include(class_includes)
+
+  end
+
+end
